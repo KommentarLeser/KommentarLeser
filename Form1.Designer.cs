9de75372522502aae338e54f95bd46359f0311f5
KommentarLeser <>
2016-07-31 10:35:47 +0200

    start

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,103 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.loadButton = new System.Windows.Forms.Button();
+			this.exitButton = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.expandButton = new System.Windows.Forms.Button();
+			this.textBoxUrl = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// treeView1
+			// 
+			this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.treeView1.Location = new System.Drawing.Point(12, 46);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(372, 611);
+			this.treeView1.TabIndex = 0;
+			this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+			// 
+			// loadButton
+			// 
+			this.loadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.loadButton.Location = new System.Drawing.Point(942, 13);
+			this.loadButton.Name = "loadButton";
+			this.loadButton.Size = new System.Drawing.Size(171, 23);
+			this.loadButton.TabIndex = 1;
+			this.loadButton.Text = "laden";
+			this.loadButton.UseVisualStyleBackColor = true;
+			this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+			// 
+			// exitButton
+			// 
+			this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.exitButton.Location = new System.Drawing.Point(942, 566);
+			this.exitButton.Name = "exitButton";
+			this.exitButton.Size = new System.Drawing.Size(171, 91);
+			this.exitButton.TabIndex = 2;
+			this.exitButton.Text = "Exit";
+			this.exitButton.UseVisualStyleBackColor = true;
+			this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(391, 46);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox1.Size = new System.Drawing.Size(545, 611);
+			this.textBox1.TabIndex = 3;
+			// 
+			// expandButton
+			// 
+			this.expandButton.Location = new System.Drawing.Point(13, 13);
+			this.expandButton.Name = "expandButton";
+			this.expandButton.Size = new System.Drawing.Size(75, 23);
+			this.expandButton.TabIndex = 4;
+			this.expandButton.Text = "Alles auf";
+			this.expandButton.UseVisualStyleBackColor = true;
+			this.expandButton.Click += new System.EventHandler(this.expandButton_Click);
+			// 
+			// textBoxUrl
+			// 
+			this.textBoxUrl.Location = new System.Drawing.Point(94, 15);
+			this.textBoxUrl.Name = "textBoxUrl";
+			this.textBoxUrl.Size = new System.Drawing.Size(842, 20);
+			this.textBoxUrl.TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1125, 669);
+			this.Controls.Add(this.textBoxUrl);
+			this.Controls.Add(this.expandButton);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.exitButton);
+			this.Controls.Add(this.loadButton);
+			this.Controls.Add(this.treeView1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.Button loadButton;
+		private System.Windows.Forms.Button exitButton;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button expandButton;
+		private System.Windows.Forms.TextBox textBoxUrl;
 	}
 }
 
