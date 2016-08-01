0746f798f62ca0eec268b077af4c7c0f9542c2eb
KommentarLeser <>
2016-08-01 18:31:50 +0200

    v1.0.0.3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,24 +34,27 @@
 			this.expandButton = new System.Windows.Forms.Button();
 			this.textBoxUrl = new System.Windows.Forms.TextBox();
 			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label1 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// treeView1
 			// 
 			this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-			this.treeView1.Location = new System.Drawing.Point(12, 46);
+			this.treeView1.HideSelection = false;
+			this.treeView1.Location = new System.Drawing.Point(12, 70);
 			this.treeView1.Name = "treeView1";
-			this.treeView1.Size = new System.Drawing.Size(372, 611);
+			this.treeView1.Size = new System.Drawing.Size(372, 655);
 			this.treeView1.TabIndex = 0;
+			this.treeView1.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeSelect);
 			this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+			this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
 			// 
 			// loadButton
 			// 
-			this.loadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.loadButton.Location = new System.Drawing.Point(1041, 13);
+			this.loadButton.Location = new System.Drawing.Point(9, 12);
 			this.loadButton.Name = "loadButton";
-			this.loadButton.Size = new System.Drawing.Size(72, 23);
+			this.loadButton.Size = new System.Drawing.Size(97, 23);
 			this.loadButton.TabIndex = 1;
 			this.loadButton.Text = "laden";
 			this.loadButton.UseVisualStyleBackColor = true;
@@ -59,31 +62,33 @@
 			// 
 			// exitButton
 			// 
-			this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.exitButton.Location = new System.Drawing.Point(1041, 631);
+			this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.exitButton.Location = new System.Drawing.Point(1041, 15);
 			this.exitButton.Name = "exitButton";
 			this.exitButton.Size = new System.Drawing.Size(72, 26);
 			this.exitButton.TabIndex = 2;
-			this.exitButton.Text = "Exit";
+			this.exitButton.Text = "beenden";
 			this.exitButton.UseVisualStyleBackColor = true;
 			this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
 			// 
 			// expandButton
 			// 
-			this.expandButton.Location = new System.Drawing.Point(13, 13);
+			this.expandButton.Location = new System.Drawing.Point(9, 41);
 			this.expandButton.Name = "expandButton";
-			this.expandButton.Size = new System.Drawing.Size(75, 23);
+			this.expandButton.Size = new System.Drawing.Size(97, 23);
 			this.expandButton.TabIndex = 4;
-			this.expandButton.Text = "Alles auf";
+			this.expandButton.Text = "Alles aufklappen";
 			this.expandButton.UseVisualStyleBackColor = true;
 			this.expandButton.Click += new System.EventHandler(this.expandButton_Click);
 			// 
 			// textBoxUrl
 			// 
 			this.textBoxUrl.AcceptsReturn = true;
-			this.textBoxUrl.Location = new System.Drawing.Point(94, 15);
+			this.textBoxUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxUrl.Location = new System.Drawing.Point(166, 15);
 			this.textBoxUrl.Name = "textBoxUrl";
-			this.textBoxUrl.Size = new System.Drawing.Size(941, 20);
+			this.textBoxUrl.Size = new System.Drawing.Size(869, 20);
 			this.textBoxUrl.TabIndex = 5;
 			this.textBoxUrl.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxUrl_KeyPress);
 			// 
@@ -92,19 +97,32 @@
 			this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.richTextBox1.Location = new System.Drawing.Point(390, 46);
+			this.richTextBox1.BackColor = System.Drawing.Color.Gainsboro;
+			this.richTextBox1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.richTextBox1.Location = new System.Drawing.Point(390, 70);
 			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(645, 611);
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(723, 655);
 			this.richTextBox1.TabIndex = 6;
 			this.richTextBox1.Text = "";
 			this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(112, 18);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(48, 13);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Adresse:";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1125, 669);
+			this.ClientSize = new System.Drawing.Size(1125, 737);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.richTextBox1);
 			this.Controls.Add(this.textBoxUrl);
 			this.Controls.Add(this.expandButton);
@@ -112,7 +130,8 @@
 			this.Controls.Add(this.loadButton);
 			this.Controls.Add(this.treeView1);
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "Sakers Kommentarleser";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
 			this.PerformLayout();
@@ -127,6 +146,7 @@
 		private System.Windows.Forms.Button expandButton;
 		private System.Windows.Forms.TextBox textBoxUrl;
 		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label1;
 	}
 }
 
